48f19dcfaaf30846114ddb226305dc52ed94dd3b
NQTrung08 <nqtrung0810@gmail.com>
2024-03-20 13:28:22 +0700

    remove api /auth & update constants

--- routes/users/authWithGithub.ts
+++
@@ -1,9 +0,0 @@
-import { type Request, type Response } from 'express'
-
-const clientId = 'd3c3bd8c6bcc882f315c'
-
-export default (req: Request, res: Response) => {
-  res.redirect(
-    `https://github.com/login/oauth/authorize?clientId=${clientId}`
-  )
-}
--- routes/users/loginWithGithub.ts
+++ routes/users/loginWithGithub.ts
@@ -1,31 +1,27 @@
 import { type Request, type Response } from 'express'
 import axios from 'axios'
-
-const clientId = 'd3c3bd8c6bcc882f315c'
-const clientSecret = 'aac335b918bb8bfa0e86d34c7e94dfadcd32a535'
+import User from '../../models/user'
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '../../utils/constants'
 
 export default async (req: Request, res: Response) => {
   try {
     const code = req.query.code
 
     const body = {
-      clientId,
-      clientSecret,
+      // eslint-disable-next-line
+      client_id: GITHUB_CLIENT_ID,
+      // eslint-disable-next-line
+      client_secret: GITHUB_CLIENT_SECRET,
       code
     }
-
     const opts = { headers: { accept: 'application/json' } }
     const response = await axios.post(
       'https://github.com/login/oauth/access_token',
       body,
       opts
     )
-    // eslint-disable-next-line
-		console.log(response.data);
     const accessToken = response.data.access_token
 
-    res.redirect(`/?access_token=${accessToken}`)
-    // Lấy thông tin người dùng từ GitHub
     const githubUserResponse = await axios.get('https://api.github.com/user', {
       headers: {
         Authorization: `token ${accessToken}`
@@ -33,11 +29,25 @@ export default async (req: Request, res: Response) => {
     })
 
     const githubUserData = githubUserResponse.data
-    // eslint-disable-next-line
-		console.log(githubUserData);
-    // console.log(githubUserData.email);
-    // let user = await User.findOne({ where: { githubId: githubUserData.id } });
-    // res.status(200).json({ message: "Đăng nhập thành công" ,user});
+
+    let user = await User.findOne({ where: { providerId: githubUserData.id } })
+
+    if (user == null) {
+      const email = `github.${githubUserData.id}@study.together`
+      const randomPassword = Math.random().toString(36).slice(-8)
+
+      user = await User.create({
+        email,
+        password: randomPassword,
+        providerId: githubUserData.id,
+        provider: 'github'
+      })
+    }
+    res.cookie('token', 'token', {
+      maxAge: 9000,
+      httpOnly: true
+    })
+    res.redirect('/')
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' })
   }
