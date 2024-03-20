f96261adedd18c0c1995bbb2e38e4369919ea96a
Learn Forward 2023 <learnforward2023@gmail.com>
2024-03-20 17:10:53 +0900

    Update client and secret github use multiple environment

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,5 +2,9 @@
 // see: https://datatracker.ietf.org/doc/html/rfc2822#section-3.4.1 for more details
 export const REGEX_EMAIL = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 export const SALT_ROUNDS = 12
-export const GITHUB_CLIENT_ID = 'd3c3bd8c6bcc882f315c'
-export const GITHUB_CLIENT_SECRET = 'aac335b918bb8bfa0e86d34c7e94dfadcd32a535'
+
+const GITHUB_CLIENT_ID_LOCAL = 'd3c3bd8c6bcc882f315c'
+const GITHUB_CLIENT_SECRET_LOCAL = 'aac335b918bb8bfa0e86d34c7e94dfadcd32a535'
+
+export const GITHUB_CLIENT_ID = process.env.NODE_ENV === 'local' ? GITHUB_CLIENT_ID_LOCAL : process.env.GITHUB_CLIENT_ID
+export const GITHUB_CLIENT_SECRET = process.env.NODE_ENV === 'local' ? GITHUB_CLIENT_SECRET_LOCAL : process.env.GITHUB_CLIENT_SECRET
