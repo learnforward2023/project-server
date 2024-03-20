d445eff7f1680e77e7cd1b48f158457508ec247c
Learn Forward 2023 <learnforward2023@gmail.com>
2024-03-20 18:50:35 +0900

    Refactor api structure

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -3,6 +3,6 @@ import registerNewAccount from './registerNewAccount'
 import loginWithGithub from './loginWithGithub'
 
 export default (router: Router) => {
-  router.post('/api/users/register', registerNewAccount)
-  router.get('/auth/github/callback', loginWithGithub)
+  router.post('/api/v1/users/register', registerNewAccount)
+  router.get('/api/v1/auth/github/callback', loginWithGithub)
 }
