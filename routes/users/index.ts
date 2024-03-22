b3a51c417bc9c76f5cbc9b8ecf1f67c1c44a2191
NQTrung08 <nqtrung0810@gmail.com>
2024-03-22 17:39:04 +0700

    feat: login with google

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,8 +1,16 @@
 import { type Router } from 'express'
 import registerNewAccount from './registerNewAccount'
 import loginWithGithub from './loginWithGithub'
+import loginWithGoogle from './loginWithGoogle'
+import { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } from '../../utils/constants'
 
 export default (router: Router) => {
   router.post('/api/v1/users/register', registerNewAccount)
   router.get('/api/v1/auth/github/callback', loginWithGithub)
+  router.get('/login', (req, res) => {
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=profile email&access_type=offline`
+
+    res.redirect(url)
+  })
+  router.get('/api/v1/auth/google/callback', loginWithGoogle)
 }
---
+++ routes/users/loginWithGoogle.ts
@@ -0,0 +1,66 @@
+import { type Request, type Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+import axios from 'axios'
+import User from '../../models/user'
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_URI,
+  SECRET_JWT_KEY
+} from '../../utils/constants'
+import { ApplicationConfig } from '../../utils/config'
+
+export default async (req: Request, res: Response) => {
+  const { code } = req.query
+
+  try {
+    const { data } = await axios.post('https://oauth2.googleapis.com/token', {
+      // eslint-disable-next-line
+      client_id: GOOGLE_CLIENT_ID,
+      // eslint-disable-next-line
+      client_secret: GOOGLE_CLIENT_SECRET,
+      code,
+      // eslint-disable-next-line
+      redirect_uri: GOOGLE_REDIRECT_URI,
+      // eslint-disable-next-line
+      grant_type: "authorization_code",
+    })
+
+    const accessToken = data.access_token
+    const googleUserResponse = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${accessToken}`
+    )
+
+    const googleUserData = googleUserResponse.data
+
+    let user = await User.findOne({ where: { providerId: googleUserData.id } })
+
+    if (user == null) {
+      const randomPassword = Math.random().toString(36).slice(-8)
+
+      user = await User.create({
+        email: googleUserData.email,
+        name: googleUserData.name,
+        password: randomPassword,
+        providerId: googleUserData.id,
+        provider: 'google'
+      })
+    }
+    const jwtToken = jwt.sign(
+      {
+        email: user.email,
+        provider: user.provider,
+        id: user.id
+      },
+      SECRET_JWT_KEY!,
+      { expiresIn: '30d' }
+    )
+
+    res.redirect(
+      `${ApplicationConfig.authRedirectSuccessUrl}?token=${jwtToken}`
+    )
+  } catch (error) {
+    res.status(500).send('An error occurred')
+  }
+}
