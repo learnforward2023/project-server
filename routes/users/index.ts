2f14f64d79a1f3eb2d9b8246c834edd12c9c035a
Learn Forward 2023 <learnforward2023@gmail.com>
2024-03-23 13:13:46 +0900

    Feat: add login feature

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -3,9 +3,11 @@ import registerNewAccount from './registerNewAccount'
 import loginWithGithub from './loginWithGithub'
 import loginWithGoogle from './loginWithGoogle'
 import loginWithFacebook from './loginWithFacebook'
+import loginWithNormalAccount from './loginWithNormalAccount'
 
 export default (router: Router) => {
   router.post('/api/v1/users/register', registerNewAccount)
+  router.post('/api/v1/users/login', loginWithNormalAccount)
   router.get('/api/v1/auth/github/callback', loginWithGithub)
   router.get('/api/v1/auth/google/callback', loginWithGoogle)
   router.get('/api/v1/auth/facebook/callback', loginWithFacebook)
